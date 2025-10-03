d602f4f1b2d5c391919e24dbd29e7f9323f585d1
oguzduzgun2525 <oguz.duzgun.2525@gmail.com>
2025-10-04 01:49:56 +0300

    Production configuration complete

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,9 +1,26 @@
-export default [
-  'strapi::logger',
+module.exports = [
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: false,
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      headers: '*',
+      origin: [
+        'http://localhost:3000',
+        'http://localhost:1337',
+        'https://avukatlik-sitesi.vercel.app',
+        'https://avukatlik-sitesi-o6oq8xwqq-oguzhan-duzguns-projects.vercel.app'
+      ]
+    }
+  },
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
--- config/server.ts
+++ config/server.ts
@@ -1,5 +1,7 @@
-export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
+module.exports = ({ env }) => ({
+  url: 'https://avukatlik-sitesi.vercel.app',
+  proxy: true,
+  host: '0.0.0.0',
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
